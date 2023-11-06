7e32e29662ba93d4a41bfe5f1848e58dfe8ff354
Marllon Ramos Marins <marllonsteam@gmail.com>
2023-11-06 09:25:46 -0800

    CellArea

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,23 +1,14 @@
-'use client'
-
-import Button from "@/components/shared/Button";
-import Card from "@/components/shared/Card";
-import Modal from "@/components/shared/Modal";
+import CellArea from "@/components/game/CellArea";
+import { PlayerType } from "core";
 
 export default function Home() {
   return (
     <div>
-      <Card color="light" noBorder>
-        test card component
-      </Card>
-
-      <Modal visible={false}>
-        test modal component
-      </Modal>
-
-      <Button onClick={()=>console.log('click test')} color="secondary">
-        test button component
-      </Button>
+     <CellArea type={PlayerType.X} selected/>
+     <CellArea type={PlayerType.O} selected/>
+     <CellArea type={PlayerType.X} />
+     <CellArea type={PlayerType.O} />
+     <CellArea />
     </div>
   );
 }
